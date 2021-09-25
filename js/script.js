4ffad54cff1cc4454b84fa168a757813d6ed9588
YuliiaKarnaukh <juliakarnaukh69@gmail.com>
2021-09-25 19:33:51 +0100

    my site

---
+++ js/script.js
@@ -0,0 +1,38 @@
+$('.single-item').slick();
+
+let mouseCursor = document.querySelector('.cursor');
+let menuNav = document.querySelectorAll('.menuNav .menu')
+window.addEventListener('mousemove', cursor);
+
+function cursor(e){
+  mouseCursor.style.top = e.pageY + 'px';
+  mouseCursor.style.left = e.pageX + 'px';
+}   
+
+// -> mouse-hover
+// .menu - .footerLinks a - .emailFooter -> hover-content
+
+document.querySelectorAll('.hover-content').forEach(link => {
+  link.addEventListener('mouseleave',() =>{
+    mouseCursor.classList.remove('mouse-hover');
+  });
+    link.addEventListener('mouseover',() =>{
+    mouseCursor.classList.add('mouse-hover');
+  });
+});
+
+document.querySelector('.colorCursor').addEventListener('mouseleave', () => {
+  mouseCursor.classList.remove('black-mouse');
+});
+
+document.querySelector('.colorCursor').addEventListener('mouseover', () => {
+   mouseCursor.classList.add('black-mouse');
+});
+
+document.querySelector('.img-about').addEventListener('mouseleave', () => {
+  mouseCursor.style.zIndex = 0;
+});
+
+document.querySelector('.img-about').addEventListener('mouseover', () => {
+   mouseCursor.style.zIndex = 10;
+});
